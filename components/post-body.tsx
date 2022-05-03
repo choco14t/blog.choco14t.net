a2c299931eccb62822e9cda411e4959a3b6b4f05
choco <chocoh14t@gmail.com>
2022-05-04 03:32:13 +0900

    feat: syntax highlighting
    - modify hovering link color

--- components/post-body.tsx
+++ components/post-body.tsx
@@ -1,17 +1,71 @@
+import ReactMarkdown from 'react-markdown'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { nord } from 'react-syntax-highlighter/dist/cjs/styles/prism'
+import { CodeComponent } from 'react-markdown/lib/ast-to-react'
+import styled from '@emotion/styled'
+import NordTheme from '../lib/nord-theme'
 import markdownStyles from './markdown-styles.module.css'
 
 type Props = {
   content: string
 }
 
+const CodeBlockWrapper = styled.div`
+  position: relative;
+`
+
+const CodeBlockTitle = styled.div`
+  background: ${NordTheme.nord3};
+  display: table;
+  color: ${NordTheme.nord4};
+  padding: 0 0.5em;
+`
+
+const InlineCodeBloack = styled.code`
+  background: rgba(0, 0, 0, 0.2);
+  border-radius: 0;
+  padding: 0 0.2em;
+`
+
+const CodeBlock: CodeComponent = ({
+  inline,
+  className,
+  children,
+  ...props
+}) => {
+  const match = /language-(\w+)(:.+)/.exec(className || '')
+  const title = match && match[2] ? match[2].slice(1) : ''
+
+  return !inline && match ? (
+    <CodeBlockWrapper>
+      <CodeBlockTitle>{title}</CodeBlockTitle>
+      <SyntaxHighlighter
+        style={nord}
+        language={match[1]}
+        customStyle={{
+          border: '1px solid #4c566a',
+          marginTop: 0,
+          borderRadius: 0,
+        }}
+      >
+        {String(children).replace(/\n$/, '')}
+      </SyntaxHighlighter>
+    </CodeBlockWrapper>
+  ) : (
+    <InlineCodeBloack className={className} {...props}>
+      {children}
+    </InlineCodeBloack>
+  )
+}
+
 const PostBody = ({ content }: Props) => {
   return (
-    <div className="max-w-2xl mx-auto">
-      <div
-        className={markdownStyles['markdown']}
-        dangerouslySetInnerHTML={{ __html: content }}
-      />
-    </div>
+    <ReactMarkdown
+      className={markdownStyles['markdown']}
+      components={{ code: CodeBlock }}
+    >
+      {content}
+    </ReactMarkdown>
   )
 }
 
--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,54 +1,39 @@
 import { useRouter } from 'next/router'
 import ErrorPage from 'next/error'
-import Container from '../../components/container'
+import Head from 'next/head'
 import PostBody from '../../components/post-body'
-import Header from '../../components/header'
 import PostHeader from '../../components/post-header'
 import Layout from '../../components/layout'
-import { getPostBySlug, getAllPosts } from '../../lib/api'
 import PostTitle from '../../components/post-title'
-import Head from 'next/head'
-import { CMS_NAME } from '../../lib/constants'
-import markdownToHtml from '../../lib/markdownToHtml'
+import { getPostBySlug, getAllPosts } from '../../lib/api'
 import PostType from '../../types/post'
 
 type Props = {
   post: PostType
-  morePosts: PostType[]
-  preview?: boolean
 }
 
-const Post = ({ post, morePosts, preview }: Props) => {
+const Post = ({ post }: Props) => {
   const router = useRouter()
+
   if (!router.isFallback && !post?.slug) {
     return <ErrorPage statusCode={404} />
   }
+
   return (
-    <Layout preview={preview}>
-      <Container>
-        <Header />
-        {router.isFallback ? (
-          <PostTitle>Loading…</PostTitle>
-        ) : (
-          <>
-            <article className="mb-32">
-              <Head>
-                <title>
-                  {post.title} | Next.js Blog Example with {CMS_NAME}
-                </title>
-                <meta property="og:image" content={post.ogImage.url} />
-              </Head>
-              <PostHeader
-                title={post.title}
-                coverImage={post.coverImage}
-                date={post.date}
-                author={post.author}
-              />
-              <PostBody content={post.content} />
-            </article>
-          </>
-        )}
-      </Container>
+    <Layout>
+      {router.isFallback ? (
+        <PostTitle>Loading…</PostTitle>
+      ) : (
+        <>
+          <article className="mb-32">
+            <Head>
+              <title>{post.title} | blog.choco14t.net</title>
+            </Head>
+            <PostHeader title={post.title} />
+            <PostBody content={post.content} />
+          </article>
+        </>
+      )}
     </Layout>
   )
 }
@@ -71,14 +56,10 @@ export async function getStaticProps({ params }: Params) {
     'ogImage',
     'coverImage',
   ])
-  const content = await markdownToHtml(post.content || '')
 
   return {
     props: {
-      post: {
-        ...post,
-        content,
-      },
+      post,
     },
   }
 }
