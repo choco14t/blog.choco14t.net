be6df5e6bd2be0a5964bd55b376f9d65bd4c4c3f
choco <chocoh14t@gmail.com>
2022-05-04 05:04:05 +0900

    build: update react and react-dom
    and delete unnecessary component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import { AppProps } from 'next/app'
 import theme from '../lib/theme'
 import '../styles/index.css'
 
-
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
